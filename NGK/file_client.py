b966c63af3601ee2ab6fb39f71a300103fd029f1
xKatze <hejhjepower@gmail.com>
2019-10-09 16:41:19 +0200

    file_client update

--- NGK/file_client.py
+++ NGK/file_client.py
@@ -12,7 +12,7 @@ def main(argv):
     print(sys.argv[1])
     client.connect((sys.argv[1],PORT))
     #client.connect((sys.argv[1:],PORT))
-    receiveFile(sys.argv[2], client)
+    #receiveFile(sys.argv[2], client)
 
 def receiveFile(fileName,  conn):
 	# TO DO Your Code

4b0dd4d8127ae1291a6dec2954e04be19aeb8030
xKatze <hejhjepower@gmail.com>
2019-10-09 16:48:18 +0200

    file_client update

--- NGK/file_client.py
+++ NGK/file_client.py
@@ -9,7 +9,7 @@ BUFSIZE = 1000
 def main(argv):
 	# TO DO Your Code
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print(sys.argv[1])
+    #print(sys.argv[1])
     client.connect((TARGET,PORT))
     #client.connect((sys.argv[1:],PORT))
     #receiveFile(sys.argv[2], client)

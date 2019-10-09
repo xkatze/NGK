9cd385467684c9d1b31a6a46d97781b0b074f958
xKatze <hejhjepower@gmail.com>
2019-10-09 17:07:30 +0200

    file_client update

--- NGK/file_client.py
+++ NGK/file_client.py
@@ -10,14 +10,14 @@ def main(argv):
 	# TO DO Your Code
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #print(sys.argv[1])
-    client.connect((TARGET,PORT))
+    client.connect((sys.argv[1],PORT))
     #client.connect((sys.argv[1:],PORT))
-    #receiveFile(sys.argv[2], client)
+    receiveFile(sys.argv[2], client)
 
 def receiveFile(fileName,  conn):
 	# TO DO Your Code
-    writeTextTCP(fileName, conn)
-    readTextTCP(conn)
+    Lib.writeTextTCP(fileName, conn)
+    Lib.readTextTCP(conn)
 
 if __name__ == "__main__":
    main(sys.argv[1:])

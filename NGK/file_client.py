c0abff6f654041defc9ff01aaff8c0f97a5e47fa
xKatze <hejhjepower@gmail.com>
2019-10-09 15:51:51 +0200

    hello

--- NGK/file_client.py
+++ NGK/file_client.py
@@ -11,9 +11,12 @@ def main(argv):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((argv[1:],PORT))
 
+    receiveFile(argv[2:], client)
+
 def receiveFile(fileName,  conn):
 	# TO DO Your Code
-    
+    lib.writeTextTCP(fileName, conn)
+    lib.readTextTCP(conn)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
